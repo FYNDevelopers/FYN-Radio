32c048e2152e0177f2ba07ef9f730e073ef780ed
FYNDevelopers <53409080+FYNDevelopers@users.noreply.github.com>
2020-01-15 09:34:36 +0300

    Update sw-fynradio.js

--- sw-fynradio.js
+++ sw-fynradio.js
@@ -47,7 +47,7 @@ var filesToCache = [
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('/service-worker.js').then(function (registration) {
+        navigator.serviceWorker.register('/sw-fynradio.js').then(function (registration) {
             console.log('Service worker successfully registered on scope', registration.scope);
         }).catch(function (error) {
             console.log('Service worker failed to register', error);
