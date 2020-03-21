c3cffef0a19774f288051e5e415e1a661190fb80
FYNDevelopers <53409080+FYNDevelopers@users.noreply.github.com>
2020-03-21 11:54:58 +0300

    Update fynradio_scripts.js

--- scripts/fynradio_scripts.js
+++ scripts/fynradio_scripts.js
@@ -67,3 +67,79 @@ var Message = document.getElementById("Message").value
 }
 
 
+
+
+
+
+
+ 
+
+
+//free trial request
+function freeTrial(){
+
+var ft_Name = document.getElementById("ft_Name").value
+var ft_email = document.getElementById("ft_email").value
+var ft_Server = document.getElementById("ft_Server").value
+var ft_Station_Name = document.getElementById("ft_Station_Name").value
+var ft_Station_description = document.getElementById("ft_Station_description").value
+ 
+    if (
+      ft_Name.length<3 || 
+      ft_Station_Name.length<3 ||
+      ft_Station_description.length<3
+      ) {
+
+        alert('Please fill in all details')
+      return false
+    }
+
+  var emailID = ft_email
+  atpos = emailID.indexOf("@")
+  dotpos = emailID.lastIndexOf(".")
+    if (atpos < 1 || ( dotpos - atpos < 2 ))
+    {
+        alert("Please enter correct email")
+        return false
+    }
+
+
+
+
+
+
+  else {
+
+    const scriptTrialFYNradio = 'https://script.google.com/macros/s/AKfycbwL-8bwYl58YefAp9-9baOGTYQWHlhUbuqVVbj_dchQ-JMP8-RW/exec?action=trial'
+    var userRequestString = scriptTrialFYNradio+
+                              '&ft_Name='+ft_Name+
+                              '&ft_email='+ft_email+
+                              '&ft_Server='+ft_Server+
+                              '&ft_Station_Name='+ft_Station_Name+
+                              '&ft_Station_description='+ft_Station_description
+
+
+    var xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function() { 
+          if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+              var res = JSON.parse(this.responseText)
+
+            if (res == 'Application Received') {
+              sessionStorage.setItem("ApplicantName", ft_Name)
+              alert(
+                'Dear ' + ft_Name + ', \n Your requst has been sent. We will use your email to contact you' 
+
+                )
+          }
+          
+      }
+      xmlHttp.open("GET", userRequestString, true); // true for asynchronous 
+      xmlHttp.send();
+    
+  }
+
+
+
+}
+
+
